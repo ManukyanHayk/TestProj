7ccb63965aee3e8861dde23d5af081550e4842d3
unknown <Edgar.Torosyan@Sis2w096.arm.synisys.com>
2014-11-27 12:16:49 +0400

    tst

---
+++ src/com/dev/edo/TestItOut.java
@@ -0,0 +1,10 @@
+package com.dev.edo;
+
+/**
+ * Created by Edgar.Torosyan on 11/27/2014.
+ */
+public class TestItOut {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
